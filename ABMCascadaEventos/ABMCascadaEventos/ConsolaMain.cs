0b248b3169ee3ff30bfe81b03dcb6f8a0747e4c5
DAR-LAB-MA\UCSE <a>
2022-05-11 18:42:45 -0300

    Commit Primero

---
+++ ABMCascadaEventos/ABMCascadaEventos/ConsolaMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMCascadaEventos
+{
+
+    public class ConsolaMain
+    {
+        // Cree una solución que utilice el patrón en capas(presentación, lógica de negocio y persistencia).
+
+        //Realice el ABM(Alta, baja, modificación) de una entidad al azar(Persona, Vehículo, etc) donde:
+
+        //-	En la aplicación de presentación se pidan los datos para cargar(al menos 4 propiedades)
+        //-	En la aplicación de lógica de negocio una clase Singleton para implementar todos los métodos
+        //-	En la aplicación de persistencia una clase Singleton para guardar en disco(formato JSON)
+
+        //Todos los métodos deben retornar void, y las capas deben comunicarse a través de eventos.
+
+        //Ir imprimiendo en pantalla los datos de la entidad registrada usando manejadores de eventos.
+
+        //Realice un método de búsqueda de entidades en la lógica de negocio, que retorne la lista a imprimir pero a través de eventos.
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ ABMCascadaEventos/LogicaABM/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaABM
+{
+    public enum Asignaturas
+    {
+        MATEMATICA, LENGUA, CIENCIAS, PROGRAMACION
+    }
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public Asignaturas Asignatura { get; set; }
+        public bool Aprobado { get; set; }
+        public int Nota { get; set; }
+
+        public Persona(string nombre, Asignaturas asignatura, int nota)
+        {
+            Nombre = nombre;
+            Asignatura = asignatura;
+            Aprobado = nota > 5 ? true : false;
+            Nota = nota;
+        }
+
+    }
+}
